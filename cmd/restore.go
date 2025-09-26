bcec744fb05c22bea6f6d7452f09515d94e734fc
Oleksii Kurinnyi <okurinny@redhat.com>
2025-09-26 18:38:30 +0300

    legal: add Eclipse Public License 2.0 headers to all Go files (#7)
    Added EPL-2.0 license headers with Red Hat copyright to all 18 Go files:
    - Copyright (c) 2025 Red Hat, Inc.
    - SPDX-License-Identifier: EPL-2.0
    - Eclipse Public License 2.0 reference and contributor information
    
    This ensures all source code is properly licensed under EPL-2.0
    and includes the required Red Hat copyright and license notices.

--- cmd/restore.go
+++ cmd/restore.go
@@ -1,5 +1,17 @@
 // Package cmd provides command line interface commands for kubectx-manager.
 // It includes commands for restoring kubernetes contexts from backup files.
+//
+// Copyright (c) 2025 Red Hat, Inc.
+// This program and the accompanying materials are made
+// available under the terms of the Eclipse Public License 2.0
+// which is available at https://www.eclipse.org/legal/epl-2.0/
+//
+// SPDX-License-Identifier: EPL-2.0
+//
+// Contributors:
+//   Red Hat, Inc. - initial API and implementation
+//
+
 package cmd
 
 import (
--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -1,5 +1,17 @@
 // Package logger provides structured logging utilities for kubectx-manager.
 // It supports different log levels and colored output for better readability.
+//
+// Copyright (c) 2025 Red Hat, Inc.
+// This program and the accompanying materials are made
+// available under the terms of the Eclipse Public License 2.0
+// which is available at https://www.eclipse.org/legal/epl-2.0/
+//
+// SPDX-License-Identifier: EPL-2.0
+//
+// Contributors:
+//   Red Hat, Inc. - initial API and implementation
+//
+
 package logger
 
 import (
